9b33b1459442809851d50fdfe38008bdf62e0ad2
Nikolay Shebanov <nikolay.shebanov@gmail.com>
2018-02-04 16:15:24 +0300

    Fix picture class ordering

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def picture_classes_for_form(project)
-    project.picture_classes.collect.with_index do |picture_class, index|
+    project.picture_classes.order('picture_classes.id asc').collect.with_index do |picture_class, index|
       ["#{index + 1}. #{picture_class.name}", picture_class.id]
     end
   end
