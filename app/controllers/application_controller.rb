0c3687aef513387015671806e4b704051274a347
Nikolay Shebanov <nikolay.shebanov@gmail.com>
2018-02-04 17:22:36 +0300

    Add basic http auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+  http_basic_authenticate_with name: ENV['BASIC_AUTH_NAME'].presence ||
+                                     fail('BASIC_AUTH_NAME not provided, check .env'),
+                               password: ENV['BASIC_AUTH_NAME'].presence ||
+                                         fail('BASIC_AUTH_PASSWORD not provided, check .env'),
+                               except: :index
 end
