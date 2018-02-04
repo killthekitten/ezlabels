ae01939d720b847170bea739d2cb43977be0aa97
Nikolay Shebanov <nikolay.shebanov@gmail.com>
2018-02-04 17:44:47 +0300

    Require basic auth everywhere; copypaste is dangerous

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,5 @@ class ApplicationController < ActionController::Base
   http_basic_authenticate_with name: ENV['BASIC_AUTH_NAME'].presence ||
                                      fail('BASIC_AUTH_NAME not provided, check .env'),
                                password: ENV['BASIC_AUTH_NAME'].presence ||
-                                         fail('BASIC_AUTH_PASSWORD not provided, check .env'),
-                               except: :index
+                                         fail('BASIC_AUTH_PASSWORD not provided, check .env')
 end
