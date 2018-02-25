7b833a74bd5a9bb2c113a40a89a9947b0cabd037
Wayzyk <romakorobko@gmail.com>
2018-02-25 20:59:43 +0200

    Destroy basic auth (#25)
    * Destroy basic auth
    
    * Returned callback

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,5 @@
 class ApplicationController < ActionController::Base
-  http_basic_authenticate_with name: ENV['BASIC_AUTH_NAME'].presence ||
-                                     fail('BASIC_AUTH_NAME not provided, check .env'),
-                               password: ENV['BASIC_AUTH_PASSWORD'].presence ||
-                                         fail('BASIC_AUTH_PASSWORD not provided, check .env')
-  before_action :authenticate_user! 
+
+  before_action :authenticate_user!
+  
 end
